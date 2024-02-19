e2f2d35a878a4d7932a9f19ef754c1b4ac5c91fa
Camilo Rojas <camilo.rojas@medismart.live>
2024-02-19 19:59:43 -0300

    next config rewrites

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
+const redirects = async () => [
+    {
+        source: "/Paciente/PlanSalud",
+        destination: `https://qa.masproteccionsalud.cl/Paciente/PlanSalud/`,
+        permanent: true,
+    },
+]
+
+
+const rewrites = async () => [
+    {
+        source: "/Paciente/PlanSalud",
+        destination: "https://qa.masproteccionsalud.cl",
+    },
+];
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    rewrites,
+    assetPrefix: "https://qa.masproteccionsalud.cl",
+};
 
 export default nextConfig;
