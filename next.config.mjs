b9bfd0126bb5d899038b63c2b2b429797f7af7c0
Camilo Rojas <camilo.rojas@medismart.live>
2024-04-11 16:54:21 -0400

    test dodo

--- next.config.mjs
+++ next.config.mjs
@@ -1,34 +1,16 @@
-const redirects = async () => [
-    {
-        source: "/Paciente/PlanSalud",
-        destination: `https://qa.masproteccionsalud.cl/Paciente/PlanSalud/`,
-        permanent: true,
-    },
-]
-
-
-const rewrites = async () => [
-    {
-        source: "/Paciente/PlanSalud",
-        destination: "https://qa.masproteccionsalud.cl",
-    },
-];
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // rewrites,
     async rewrites() {
         return {
             fallback: [
                 {
                     source: '/:path*',
-                    destination: `https://qa.masproteccionsalud.cl/:path*`,
+                    destination: "https://qa.dodo.medismart.live/:path*",
                 },
             ],
-            
+
         }
     },
-    // assetPrefix: "https://qa.masproteccionsalud.cl",
 };
 
 export default nextConfig;
