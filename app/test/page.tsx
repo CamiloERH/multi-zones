2df9e6abe179be54762eed38809008439afaa27d
Camilo Rojas <camilo.rojas@medismart.live>
2024-04-17 18:59:12 -0400

    iframe

---
+++ app/test/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+    return (
+        <iframe width="100%" height="800" src="https://qa.itausegurotecuidosalud.cl/" frameBorder="0" sandbox="allow-forms allow-modals allow-popups allow-scripts allow-presentation"></iframe>
+    )
+}
