24d9154b532c8211819ab706d875afb061b12867
Camilo Rojas <camilo.rojas@medismart.live>
2024-04-18 00:12:28 -0400

    test

--- next.config.mjs
+++ next.config.mjs
@@ -10,24 +10,7 @@ const nextConfig = {
             ],
 
         }
-    },
-    async headers() {
-        return [
-            {
-                source: '/:path*',
-                headers: [
-                    {
-                        key: 'Content-Security-Policy',
-                        value: 'frame-ancestors https://qa.medical.medismart.live',
-                    },
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "https://qa.medical.medismart.live" }, // replace this your actual origin
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                ],
-            },
-        ]
-    },
+    }
 };
 
 export default nextConfig;
